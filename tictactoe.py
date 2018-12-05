95e5f177ec3c31a66757723f0b40f548da2aa833
ProgrammerChris <chris.tvedt@gmail.com>
2018-12-05 13:45:05 +0100

    added win and draw check

--- tictactoe.py
+++ tictactoe.py
@@ -3,9 +3,7 @@
 import pygame
 import os
 
-# TODO: Write win check
 # TODO: Display winner and close or restart game
-
 
 pygame.init()
 
@@ -26,37 +24,68 @@ grid =    (
 
 # Current board
 board = [
-        '-', '-', '-',
-        '-', '-', '-',
-        '-', '-', '-'
+        '_', '_', '_',
+        '_', '_', '_',
+        '_', '_', '_'
         ]
 
 # Drawing grid
 for square in grid:
     pygame.draw.rect(screen, (255,255,255), square, 1)
 
-# Refresh display
+# Updates screen
 pygame.display.flip()
 
 # Keep display showing until closed.
 running = True
 
+def check_result():
+    winner = ''
+
+    if (    board[3] == board[4] == board[5] != '_' or # Middle row winner
+            board[2] == board[4] == board[6] != '_' or # Upwards diagonal winner
+            board[1] == board[4] == board[7] != '_' or # Middle column winner
+            board[0] == board[4] == board[8] != '_' # Downwards diagonal winner 
+        ):
+
+        winner = board[4]
+        print('Winner', winner)
+        return winner
+
+    elif (  board[6] == board[7] == board[8] != '_' or # Bottom winner
+            board[2] == board[5] == board[8] != '_' # Right column winner
+        ):
+        winner = board[8]
+        print('Winner', winner)
+        return winner
+    elif (  board[0] == board[1] == board[2] != '_' or # Top row winner
+            board[0] == board[3] == board[6] != '_' # Left column winner
+        ):
+        winner = board[0]
+        print('Winner', winner)
+        return winner
+    elif '_' not in board: # If full board but no winners
+        print('Draw!')
+        return None
+        
 while running:
     
     for event in pygame.event.get():
-        
+
         # Quit if 'X' pressed on window or if 'ESC' is pressed
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
+        
 
         # If mouse clicked
-        if event.type == pygame.MOUSEBUTTONUP:
+        if event.type == pygame.MOUSEBUTTONUP and check_result() == None:
             for square in grid:
+
                 # Check which square is being clicked
-                if square.collidepoint(event.pos) and board[grid.index(square)] == '-':
+                if square.collidepoint(event.pos) and board[grid.index(square)] == '_':
                     if whos_turn == 0:
                         # Draw X in square
                         pygame.draw.line(screen, (255,255,255), (square[0] + 50, square[1] + 50), (square[0] + 150, square[1] + 150), 15)
@@ -67,15 +96,23 @@ while running:
                         pygame.draw.circle(screen, (255,255,255), (square[0] + 100, square[1] + 100), 70, 10)
                         board[grid.index(square)] = 'O'
 
-                    # Clears terminal to show board more nicely as it updates. Unix/linux and windows friendly.
+                    # Clears terminal to show board more nicely in console as it updates. Unix/linux and windows friendly.
                     os.system('cls' if os.name=='nt' else 'clear')
 
                     # Print board to console. Just because.
                     print(str(board[:3]) + "\n" + str(board[3:6]) + "\n" + str(board[6:9]))
+
+                    pygame.display.flip()
+
+                    check_result()
                     
                     # Switch turns between X and O
                     whos_turn = whos_turn ^ 1
+                
 
-                pygame.display.flip()
+    
 
-pygame.quit()
+
+
+
+
